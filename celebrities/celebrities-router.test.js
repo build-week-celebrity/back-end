7879e9c42267231c7e87e7a07fd84acc0b7752fd
Joscelyn <JoscelynRose@gmail.com>
2020-01-08 18:20:13 -0500

    writing tests to make sure the test database is working

--- celebrities/celebrities-router.test.js
+++ celebrities/celebrities-router.test.js
@@ -5,4 +5,18 @@ const authenticate = require("../auth/authenticate-middleware.js");
 beforeEach(testdb);
 beforeEach(() => authenticate.mockClear());
 
-describe("celebrities", () => {});
+describe("celebrities", () => {
+  // no authentication
+  it("get /", async () => {
+    const res = await request(server).get("/");
+    console.log("testing testing 1 2 3");
+    expect(res.status).toBe(200);
+    expect(authenticate).not.toBeCalled();
+  });
+  it("get nothing", async () => {
+    const res = await request(server).get("/900");
+    console.log("testing testing 4 5 6");
+    expect(res.status).toBe(404);
+    expect(authenticate).not.toBeCalled();
+  });
+});
